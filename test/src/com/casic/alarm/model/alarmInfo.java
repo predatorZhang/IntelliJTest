44aa3cf14e454dab171a8f9df049b0d7e8313fa2
zhangfan <zhangfan1212@126.com>
2014-09-14 22:54:55 +0800

    this is my intellij first commit

---
+++ test/src/com/casic/alarm/model/alarmInfo.java
@@ -0,0 +1,48 @@
+package com.casic.alarm.model;
+
+/**
+ * Created by admin on 2014/9/14.
+ */
+public class alarmInfo
+{
+    private int id;
+    private int num;
+    private int num2;
+
+    public int getId()
+    {
+        return id;
+    }
+
+    public void setId(int id)
+    {
+        this.id = id;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public String getPassword()
+    {
+        return password;
+    }
+
+    public void setPassword(String password)
+    {
+        this.password = password;
+    }
+
+    private String name;
+    private String password;
+
+
+
+
+}
